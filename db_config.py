b84b92c7d965955626b3b63b9978813ba5c87208
Raunak Varma <raunakvarma05@gmail.com>
2023-01-04 23:22:08 +0530

    added navbar and defined structure for template inheritence

--- db_config.py
+++ db_config.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from app import app
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:shri123@localhost/twitter_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SQLALCHEMY_ECHO']=False
